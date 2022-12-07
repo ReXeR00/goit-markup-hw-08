db8c81d895cd5a578d01ce317319ddd4f414eb16
ReXeR00 <106077454+ReXeR00@users.noreply.github.com>
2022-12-07 19:35:08 +0200

    Update mobile-menu 07.12022

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,6 +2,8 @@
   const menuBtnRef = document.querySelector("[data-burger-btn]");
   const mobileMenuRef = document.querySelector("[data-menu-container]");
 
+  const body = document.getElementsByTagName("body")[0];
+
   menuBtnRef.addEventListener("click", () => {
     body.classList.toggle("modal-open");
 
